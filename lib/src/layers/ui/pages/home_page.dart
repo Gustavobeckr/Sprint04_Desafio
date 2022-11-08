5afe348e44adf8e3a91ad6f53ecc4882af2e00d6
Gustavo Becker <gustavobe0504@gmail.com>
2022-11-08 15:42:20 -0300

    feat: implementação da home page

---
+++ lib/src/layers/ui/pages/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/src/layers/ui/components/drawer_bar.dart';
+import '../components/theme_store_widget.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo List'),
+        elevation: 5,
+        actions: [
+          AnimatedBuilder(
+            animation: themeStore,
+            builder: (context, child) {
+              return Switch(
+                  value: themeStore.isDartTheme,
+                  onChanged: (value) {
+                    themeStore.shitchTheme();
+                  });
+            },
+          ),
+        ],
+      ),
+      drawer: const DrawerBar(),
+    );
+  }
+}

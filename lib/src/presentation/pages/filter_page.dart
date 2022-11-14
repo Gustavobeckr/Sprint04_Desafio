c9e39547edf2b773d7a768a3f43236c4da130084
Gustavo Becker <gustavobe0504@gmail.com>
2022-11-13 22:47:06 -0300

    feat: ações da lista de filtros

--- lib/src/presentation/pages/filter_page.dart
+++ lib/src/presentation/pages/filter_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:todo_list/src/data/services/filter/filter_model.dart';
-import 'package:todo_list/src/data/services/filter/filter_services.dart';
+import 'package:todo_list/src/data/local_data_sources/models/filter_model.dart';
+import 'package:todo_list/src/data/services/filter_services.dart';
 
 class FilterPage extends StatefulWidget {
   const FilterPage({super.key});
@@ -15,6 +15,44 @@ class _FilterPageState extends State<FilterPage> {
 
   final Filter _filter = Filter();
   final FilterServices _filterServices = FilterServices();
+
+  final List<Filter> _filterList = [];
+
+  final TextEditingController _editTitleController = TextEditingController();
+  final TextEditingController _editDescriptionController =
+      TextEditingController();
+
+  var filter;
+
+  @override
+  void initState() {
+    super.initState();
+    getAllFilters();
+  }
+
+  final GlobalKey<ScaffoldState> _globalKey = GlobalKey<ScaffoldState>();
+
+  getAllFilters() async {
+    var filters = await _filterServices.readFilter();
+    filters.forEach((filter) {
+      setState(() {
+        var filterModel = Filter();
+        filterModel.title = filter['title'];
+        filterModel.description = filter['description'];
+        filterModel.id = filter['id'];
+        _filterList.add(filterModel);
+      });
+    });
+  }
+
+  _editFilter(context, filterId) async {
+    filter = await _filterServices.readFilterById(filterId);
+    setState(() {
+      _editTitleController.text = filter[0]['title'];
+      _editTitleController.text = filter[0]['description'];
+    });
+    _formEditDailog(context);
+  }
 
   _formDailog(BuildContext context) {
     return showDialog(
@@ -45,11 +83,12 @@ class _FilterPageState extends State<FilterPage> {
                   },
                   child: const Text('Cancel')),
               TextButton(
-                  onPressed: () {
+                  onPressed: () async {
                     Navigator.pop(context);
                     _filter.title = _titleController.text;
                     _filter.description = _descriptionController.text;
-                    _filterServices.saveFilter(_filter);
+                    var result = await _filterServices.saveFilter(_filter);
+                    getAllFilters();
                   },
                   child: const Text('Confirm'))
             ],
@@ -57,17 +96,129 @@ class _FilterPageState extends State<FilterPage> {
         });
   }
 
+  _formEditDailog(BuildContext context) {
+    return showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (param) {
+          return AlertDialog(
+            title: const Text('Edit Filter'),
+            content: SingleChildScrollView(
+              child: Column(children: [
+                TextFormField(
+                  controller: _editTitleController,
+                  decoration: const InputDecoration(
+                      hintText: 'Write a title', labelText: 'Title'),
+                ),
+                TextFormField(
+                  controller: _editDescriptionController,
+                  decoration: const InputDecoration(
+                      hintText: 'Write a description',
+                      labelText: 'Description'),
+                ),
+              ]),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cancel')),
+              TextButton(
+                  onPressed: () async {
+                    _filter.id = filter[0]['id'];
+                    _filter.title = _editTitleController.text;
+                    _filter.description = _editDescriptionController.text;
+                    var result = await _filterServices.editFilter(_filter);
+
+                    if (result > 0) {
+                      Navigator.pop(context);
+                      getAllFilters();
+                    }
+                  },
+                  child: const Text('Confirm'))
+            ],
+          );
+        });
+  }
+
+  _deleteDialog(BuildContext context, filterId) {
+    return showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (param) {
+          return AlertDialog(
+            title: const Text('Remove'),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cancel')),
+              TextButton(
+                  onPressed: () async {
+                    var result = await _filterServices.deleteFilter(filterId);
+
+                    if (result > 0) {
+                      getAllFilters();
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: const Text('Confirm'))
+            ],
+          );
+        });
+  }
+
+  _successSnackBar(message) {
+    var snackBar = SnackBar(content: message);
+    _globalKey.currentState!.showBottomSheet((context) => snackBar);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _globalKey,
       appBar: AppBar(
         title: const Text('Filter Title'),
         elevation: 5,
       ),
-      body: const Center(child: Text('Titles')),
+      body: ListView.builder(
+          itemCount: _filterList.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(top: 8, left: 16, right: 16),
+              child: Card(
+                elevation: 10,
+                child: ListTile(
+                  leading: IconButton(
+                      icon: const Icon(Icons.edit),
+                      onPressed: () {
+                        _editFilter(context, _filterList[index].id);
+                      }),
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(_filterList[index].title.toString()),
+                      IconButton(
+                          onPressed: () {
+                            _deleteDialog(context, _filterList[index].id);
+                          },
+                          icon: const Icon(
+                            Icons.delete,
+                            color: Colors.red,
+                          ))
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _formDailog(context);
+          setState(() {
+            _formDailog(context);
+          });
         },
         child: const Icon(Icons.add),
       ),
